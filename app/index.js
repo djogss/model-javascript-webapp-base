775f5cf81f84b0bb02741379eb3afa6d9fc1293a
Mantas Armonas <mantas.armonas@logicalglue.com>
2017-09-22 13:11:07 +0100

    Adding scss support

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';  
 import ReactDOM from 'react-dom';  
 import WelcomeMessage from './components/WelcomeMessage.jsx';
+import SCSS from './styles/app.scss'; 
 
 const appData = {  
   title: 'React Demo App'
