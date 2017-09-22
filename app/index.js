ec10f91c05e39db56c948273612cca8bde3a5678
Mantas Armonas <mantas.armonas@logicalglue.com>
2017-09-22 13:03:44 +0100

    Adding react-js support

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');  
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import WelcomeMessage from './components/WelcomeMessage.jsx';
+
+const appData = {  
+  title: 'React Demo App'
+}
+
+ReactDOM.render(  
+  <WelcomeMessage data={appData} />,
+   document.getElementById('container')
+); 
