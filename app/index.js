6461bc993da667e4cc2df58a8f458d6f54d735ce
Mantas Armonas <mantas.armonas@logicalglue.com>
2017-09-22 13:47:33 +0100

    First working redux application

--- app/index.js
+++ app/index.js
@@ -1,23 +1,42 @@
-console.log('Hello World!');  
+console.log('Hello World!');
 import expect, { createSpy, spyOn, isSpy } from 'expect'
+import { createStore } from 'redux'
 
 const counter = (state = 0, action) => {
-    switch(action.type){
-      case  'INC': return state + 1;
-      case  'DEC': return state - 1;
-      default: return state;
+    switch (action.type) {
+        case 'INC': return state + 1;
+        case 'DEC': return state - 1;
+        default: return state;
     }
-  }
+}
 
-  expect(counter(1, {type:'INC'})).toEqual(2)
-  
-  expect(counter(1, {type:'DEC'})).toEqual(0)
-  
-  expect(counter(1, {type:'UNKNOWN'})).toEqual(1)
-  
-  expect(counter(undefined, {})).toEqual(0)
-  
-  console.log("All tests success")
+expect(counter(1, { type: 'INC' })).toEqual(2)
+
+expect(counter(1, { type: 'DEC' })).toEqual(0)
+
+expect(counter(1, { type: 'UNKNOWN' })).toEqual(1)
+
+expect(counter(undefined, {})).toEqual(0)
+
+console.log("All tests success")
 
 console.log("test success")
-  
+
+const store = createStore(counter);
+
+console.log(store.getState());
+
+const render = () => {
+    document.body.innerText = store.getState();
+}
+
+const printConsole = () => {
+    console.log(store.getState())
+}
+store.subscribe(render);
+store.subscribe(printConsole);
+render();
+
+document.addEventListener('click', () => {
+    store.dispatch({type:'INC'});
+});
